acfdb974939d35c93678cf8393cdcf4fce71064f
Michael Clausen <encodeering@gmail.com>
2019-01-20 12:59:19 +0100

    calls wsl with glue script and fs to create the proxy
    * dollar sign needs to escaped in order to avoid some pre-processing step (resolution of those variables)

--- modules/proxy/main.go
+++ modules/proxy/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+    "bytes"
+    "errors"
     "os"
+    "os/exec"
+    "strings"
+    "github.com/encodeering/wsl/proxy/internal/stick"
 )
 
 //go:generate tar -C glue -czvf ./internal/stick/res/glue.tar.gz .
 
 func main() {
-    die (nil)
+    if len (os.Args) < 1 {
+        die (errors.New ("too less arguments"))
+    }
+
+    proxy := stick.New (func (script []byte, fs []byte) (err error) {
+        cmd := exec.Command ("wsl.exe", "bash", "-c", strings.Replace (string (script), "$", "\\$", -1), "--", os.Args[1])
+        cmd.Stdin = bytes.NewReader (fs)
+        cmd.Stdout = os.Stdout
+        cmd.Stderr = os.Stderr
+
+        return cmd.Run ()
+    })
+
+    die (proxy.Process ())
 }
 
 func die (err error) {
