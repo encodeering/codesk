1eb62ff8d6b1299153cef4bfe30e7fa328f038d8
Michael Clausen <encodeering@gmail.com>
2019-01-26 11:31:40 +0100

    extracts pipe generation from process tests
    * refactoring: dry
    
    NOTE
    
    * adds a defer to close the pipe eventually

--- modules/codesk/glue/internal/process/process_test.go
+++ modules/codesk/glue/internal/process/process_test.go
@@ -1,6 +1,7 @@
 package process
 
 import (
+    "io"
     "io/ioutil"
     "os"
     "testing"
@@ -8,35 +9,45 @@ import (
 )
 
 func TestExecOkay (t *testing.T) {
-    r, w, err := os.Pipe ()
-    assert.NoError (t, err)
+    capture (t, func (r io.Reader, w io.WriteCloser, _ io.Reader, _ io.WriteCloser) {
+        assert.Equal   (t, 0, Exec ("echo", []string{"-n", "hello"}))
+        assert.NoError (t, w.Close ())
 
-    stdout := os.Stdout
-    os.Stdout = w
-    defer func () { os.Stdout = stdout } ()
+        content, err := ioutil.ReadAll (r)
 
-    assert.Equal   (t, 0, Exec ("echo", []string{"-n", "hello"}))
-    assert.NoError (t, w.Close ())
-
-    content, err := ioutil.ReadAll (r)
-
-    assert.NoError (t, err)
-    assert.Equal   (t, "hello", string (content))
+        assert.NoError (t, err)
+        assert.Equal   (t, "hello", string (content))
+    })
 }
 
 func TestExecFail (t *testing.T) {
-    r, w, err := os.Pipe ()
+    capture (t, func (_ io.Reader, _ io.WriteCloser, r io.Reader, w io.WriteCloser) {
+        assert.Equal   (t, 1, Exec ("echosounder", []string{"-n", "hello"}))
+        assert.NoError (t, w.Close ())
+
+        content, err := ioutil.ReadAll (r)
+
+        assert.NoError (t, err)
+        assert.Equal   (t, "", string (content)) // stderr still empty ?
+    })
+}
+
+func capture (t* testing.T, f func (outR io.Reader, outW io.WriteCloser, errR io.Reader, errW io.WriteCloser)) {
+    outR, outW, err := os.Pipe ()
     assert.NoError (t, err)
+    defer outW.Close ()
+
+    errR, errW, err := os.Pipe ()
+    assert.NoError (t, err)
+    defer errW.Close ()
+
+    stdout := os.Stdout
+    os.Stdout = outW
+    defer func () { os.Stdout = stdout } ()
 
     stderr := os.Stderr
-    os.Stderr = w
+    os.Stderr = errW
     defer func () { os.Stderr = stderr } ()
 
-    assert.Equal   (t, 1, Exec ("echosounder", []string{"-n", "hello"}))
-    assert.NoError (t, w.Close ())
-
-    content, err := ioutil.ReadAll (r)
-
-    assert.NoError (t, err)
-    assert.Equal   (t, "", string (content)) // stderr still empty ?
+    f (outR, outW, errR, errW)
 }
