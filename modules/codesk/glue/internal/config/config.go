35f65f8557c95b7c4603fccb13720e7c0428739a
Michael Clausen <encodeering@gmail.com>
2019-02-01 11:48:09 +0100

    defines type structures for the configuration
    * one representation shall be later given in yaml and one defined by environment settings

--- modules/codesk/glue/internal/config/config.go
+++ modules/codesk/glue/internal/config/config.go
@@ -24,3 +24,32 @@ func init () {
 
     Uhd = user.HomeDir
 }
+
+type Resolution string
+
+type Distribution struct {
+    Name string
+}
+
+type User struct {
+    Name string
+}
+
+type Box struct {
+    Distribution Distribution
+    User User
+}
+
+type Environment struct {
+    Resolution Resolution
+    Var []string
+}
+
+type Command struct {
+    Environment Environment
+}
+
+type Config struct {
+    Box Box
+    Command Command
+}
