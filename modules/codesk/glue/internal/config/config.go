37d1c3bb77ea9c1ffaa417d8c21ab8c09cc0b3a0
Michael Clausen <encodeering@gmail.com>
2019-02-05 11:49:51 +0100

    seperates init code from config file
    * refactoring: not necessary to keep them in the same file, as they handle different concerns

--- modules/codesk/glue/internal/config/config.go
+++ modules/codesk/glue/internal/config/config.go
@@ -1,30 +1,5 @@
 package config
 
-import (
-    "os"
-    "os/user"
-    "path/filepath"
-    . "github.com/encodeering/wsl/glue/internal/util"
-)
-
-var Pwd string
-var Uhd string
-
-func init () {
-    executable, err := os.Executable ()
-    Die (err)
-
-    path, err := filepath.EvalSymlinks (executable)
-    Die (err)
-
-    Pwd = filepath.Dir (path)
-
-    user, err := user.Current ()
-    Die (err)
-
-    Uhd = user.HomeDir
-}
-
 type Resolution string
 
 type Distribution struct {
