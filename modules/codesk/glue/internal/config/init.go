63e55790df4f6f7582eb44816a2b3a81c1c5ce9f
Michael Clausen <encodeering@gmail.com>
2019-02-06 12:10:14 +0100

    creates a configuration based on different sources
    * yaml in home directory - codesk.yml
    * yaml in process directory - ${binary}.yml
    * env configuration

--- modules/codesk/glue/internal/config/init.go
+++ modules/codesk/glue/internal/config/init.go
@@ -4,8 +4,27 @@ import (
     "os"
     "os/user"
     "path/filepath"
+    "strings"
     . "github.com/encodeering/wsl/glue/internal/util"
 )
+
+func Configuration () *Config {
+    arg0 := os.Args[0]
+
+    base := filepath.Base (arg0)
+    ext  := filepath.Ext  (arg0)
+
+    homeyml, err := ObtainYaml (filepath.Join (Uhd (), "codesk.yml"))
+    Die (err)
+
+    selfyml, err := ObtainYaml (filepath.Join (Pwd (), strings.TrimSuffix (base, ext) + ".yml"))
+    Die (err)
+
+    env, err := ObtainOS ("CODESK")
+    Die (err)
+
+    return Combine (homeyml, selfyml, env)
+}
 
 func Pwd () string {
     executable, err := os.Executable ()
