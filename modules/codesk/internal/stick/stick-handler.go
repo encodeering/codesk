efb6f8e25eda567135afb968a8c912971e85a66f
Michael Clausen <encodeering@gmail.com>
2019-02-13 16:30:04 +0100

    closes writer for io handle
    * transfers ownershp to io handle, client doesn't have to implement a finally task

--- modules/codesk/internal/stick/stick-handler.go
+++ modules/codesk/internal/stick/stick-handler.go
@@ -8,8 +8,10 @@ import (
     "strings"
 )
 
-func IoHandle (target string, writer io.Writer) Handle {
+func IoHandle (target string, writer io.WriteCloser) Handle {
     return func (script []byte, fs []byte) (err error) {
+        defer writer.Close ()
+
         cmd := exec.Command ("wsl.exe", "bash", "-c", strings.Replace (string (script), "$", "\\$", -1), "--", target)
         cmd.Stdin = bytes.NewReader (fs)
         cmd.Stdout = writer
